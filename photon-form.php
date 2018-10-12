c687e5eab84d3aff23b29d9359150bacbdea47bf
i-am-phi <phi@i-am-phi.net>
2018-10-11 17:21:40 -0700

    built in css js config

--- photon-form.php
+++ photon-form.php
@@ -86,13 +86,13 @@ class PhotonFormPlugin extends Plugin
         // styles
         if ($this->config->get('plugins.photon-form.built_in_css')) {
           $css = 'plugin://photon-form/assets/form.css';
-          $assets->addCss($css);
+          $assets->addCss($css, 100, false, 'photon-plugins' );
         }
 
         // scripts
         if ($this->config->get('plugins.photon-form.built_in_js')) {
           $js = 'plugin://photon-form/assets/form.js';
-          $assets->addJs($js);
+          $assets->addJs($js, 100, false, 'defer', 'photon-plugins' );
         }
 
 
